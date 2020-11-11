1e1d79c85c928651f2ddcf47a4f6408fde4eeb4a
BlackTouma <42772506+BlackTouma@users.noreply.github.com>
2020-11-11 23:18:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,4 +15,4 @@
 
 cd lede/package/lean  
 rm -rf luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
